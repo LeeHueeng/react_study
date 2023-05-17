97cd40364737f96ae31b692e7e20da4518ee12c9
Lee-hyeonu <zzxx373014@gmail.com>
2023-05-18 00:27:55 +0900

    [css적용]

--- client/src/Component/Heading.js
+++ client/src/Component/Heading.js
@@ -3,12 +3,13 @@ import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import "./Heading.css";
 
 function Heading() {
   return (
-    <Navbar bg="dark" expand="lg" variant="dark">
+    <Navbar expand="lg" variant="dark" style={{ backgroundColor: "#8064A2" }}>
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Hueeng</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
