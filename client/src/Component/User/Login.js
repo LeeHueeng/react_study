12464121264af8e815c869ca357924ec4a143bbf
Leehyeonu <zzxx373014@gmail.com>
2023-05-28 04:11:54 +0900

    회원가입 로그인 밑준비 완성

--- client/src/Component/User/Login.js
+++ client/src/Component/User/Login.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import LoginDiv from "../../style/UserCSS";
 
 function Login() {
+  const [Email, setEmail] = useState("");
+  const [Pw, setPW] = useState("");
+
+  let navigate = useNavigate();
+
   return (
     <LoginDiv>
       <label>이메일</label>
-      <input type="email" />
+      <input
+        type="email"
+        value={Email}
+        onChange={(e) => setEmail(e.currentTarget.value)}
+      />
       <br></br>
       <label>비밀번호</label>
-      <input type="password" />
+      <input
+        type="password"
+        value={Pw}
+        onChange={(e) => setPW(e.currentTarget.value)}
+      />
       <button>로그인</button>
-      <button>회원가입</button>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          navigate("/register");
+        }}
+      >
+        회원가입
+      </button>
     </LoginDiv>
   );
 }
--- client/src/Component/User/Register.js
+++ client/src/Component/User/Register.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import LoginDiv from "../../style/UserCSS";
 
 function Register() {
+  const [Name, setName] = useState("");
+  const [Email, setEmail] = useState("");
+  const [PW, setPW] = useState("");
+  const [PWConfirm, setPWConfirm] = useState("");
+
   return (
     <LoginDiv>
       <from>
         <lable>이름</lable>
-        <input type="name" />
+        <input
+          type="name"
+          value={Name}
+          onChange={(e) => setName(e.currentTarget.value)}
+        />
         <lable>이메일</lable>
-        <input type="email" />
+        <input
+          type="email"
+          value={Email}
+          onChange={(e) => setEmail(e.currentTarget.value)}
+        />
         <lable>비밀번호</lable>
-        <input type="password" />
+        <input
+          type="password"
+          value={PW}
+          onChange={(e) => setPW(e.currentTarget.value)}
+        />
         <lable>비밀번호 확인</lable>
-        <input type="password" />
+        <input
+          type="password"
+          value={PWConfirm}
+          onChange={(e) => setPWConfirm(e.currentTarget.value)}
+        />
         <button>회원가입</button>
       </from>
     </LoginDiv>
