9cfb701fac937eed70671dfb009c8bf43049ab63
Leehyeonu <zzxx373014@gmail.com>
2023-06-18 16:08:24 +0900

    중간

--- client/src/Component/Reple/RepleContent.js
+++ client/src/Component/Reple/RepleContent.js
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 function RepleContent(props) {
   const [ModalFlag, setModalFlag] = useState(false);
+  const ref = useRef();
+
   return (
     <div>
       <dix>
