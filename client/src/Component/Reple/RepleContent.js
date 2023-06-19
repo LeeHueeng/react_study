1d69624983a43c633cdd284c8f0328b96abedc0a
Leehyeonu <zzxx373014@gmail.com>
2023-06-19 10:54:50 +0900

    useOnClickOutside를 활용하여 댓글 수정 및 삭제를 누르면 보이고 다른데 클릭하면 안보이게 설정 완료

--- client/src/Component/Reple/RepleContent.js
+++ client/src/Component/Reple/RepleContent.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
 
 function RepleContent(props) {
   const [ModalFlag, setModalFlag] = useState(false);
   const ref = useRef();
-
+  const user = useSelector((state) => state.user);
+  useOnClickOutside(ref, () => setModalFlag(false));
   return (
     <div>
       <dix>
@@ -12,16 +14,17 @@ function RepleContent(props) {
             <p>{props.reple.author.displayName}</p>
             <comment>{props.reple.reple}</comment>
           </div>
-
-          <div className="modalControl">
-            <span onClick={() => setModalFlag(true)}>···</span>
-            {ModalFlag && (
-              <div className="modalDiv">
-                <p>수정</p>
-                <p className="delete">삭제</p>
-              </div>
-            )}
-          </div>
+          {props.reple.author.uid === user.uid && (
+            <div className="modalControl">
+              <span onClick={() => setModalFlag(true)}>···</span>
+              {ModalFlag && (
+                <div className="modalDiv" ref={ref}>
+                  <p>수정</p>
+                  <p className="delete">삭제</p>
+                </div>
+              )}
+            </div>
+          )}
 
           <br />
         </div>
@@ -29,4 +32,22 @@ function RepleContent(props) {
     </div>
   );
 }
+
+function useOnClickOutside(ref, handler) {
+  useEffect(() => {
+    const listener = (event) => {
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+      handler(event);
+    };
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [ref, handler]);
+}
+
 export default RepleContent;
